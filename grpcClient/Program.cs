236b34705fc437c4a49a942c52d4a6fd7a732d1e
Emre Baris Erdem <erdem.emre.baris@gmail.com>
2025-03-14 21:01:07 +0300

    Add project files.

---
+++ grpcClient/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Grpc.Net.Client;
+using grpcServer;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7151");
+
+var greetClient = new Greeter.GreeterClient(channel);
+
+HelloReply response = await greetClient.SayHelloAsync(new HelloRequest()
+{
+  Name = "Emre Barış Grpc Öğreniyor!"
+});
+
+
+Console.WriteLine(response);
+
+Console.Read();
